2dd1a1236a82416311816270107515ed8ed9b9f7
Christina Thomas <Penssake@gmail.com>
2017-10-02 18:38:44 -0700

    reading through each file only once now, still unordered/unpredictable, not returning an array

--- lab-christina/__test__/reader.test.js
+++ lab-christina/__test__/reader.test.js
@@ -4,14 +4,14 @@ const reader = require('../lib/reader.js');
 
 describe('reader', () => {
   test('an invalid path will result in an error message', (done) => {
-    reader(`${__dirname}/mkdir`, (err, data) => {
+    reader(`${__dirname}/fake`, (err, data) => {
       expect(data).toBeUndefined();
       done();
     });
   });
-
-  test('a valid path should resolve in a string', (done) => {
-    reader([`${__dirname}/asset/betcha.txt`,`${__dirname}/asset/data.txt`,`${__dirname}/asset/lastly.txt`], (err, data) => {
+  test('a valid path should resolve in an array of strings', (done) => {
+    reader([`${__dirname}/asset/betcha.txt`,`${__dirname}/asset/data.txt`, `${__dirname}/asset/lastly.txt`], (err, data) => {
+      expect(err).toBeNull();
       expect(data).toEqual(['words', 'for', 'food']);
       done();
     });
--- lab-christina/lib/reader.js
+++ lab-christina/lib/reader.js
@@ -2,13 +2,24 @@
 
 const fs = require('fs');
 
-module.exports = (path, callback) => {
-  for(var i = 0; i < path.length; i++){
-    fs.readFile(path[i], 'utf-8', (err, data) => {
+
+module.exports = (filepath, callback) => {
+  let pathArray = [];
+  for(let i = 0; i < filepath.length; i++){
+    fs.readFile(filepath[i], 'utf-8', (err, data) => {
       if (err) {
-        callback(err);
+        return callback(err);
       }
-      callback(null, data);
+      if(pathArray <= 3)
+        pathArray[i] = (data);
+      pathArray[i] + 1;
+      console.log(data);
+      if(pathArray === 3)
+        callback(null, data);
     });
   }
 };
+
+/* module.exports = (filepath, callback) => {
+
+*/
